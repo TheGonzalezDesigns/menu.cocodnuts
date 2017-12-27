a270df4a60c4eb348989e14df4ada48449e3aa40
hugo@thegonzalez.design <hugo@thegonzalez.design>
2017-12-26 23:42:02 -0800

    Redefined the validation method.

--- client/src/scripts/api/router.js
+++ client/src/scripts/api/router.js
@@ -1,44 +1,56 @@
 /*global exports*/
 import axios from 'axios'
 
-function validateStatus(status) {
-	return status >= 200 && status < 300
-}
-
-function handleError(error) {
-	let statusIsValid = true
-	if (error.response) {
-		let status = error.response.status
-		console.warn(error.response.data)
-		console.warn(status)
-		console.warn(error.response.headers)
-		statusIsValid = validateStatus(status)
-	} else if (error.request) console.warn(error.request)
-	else console.warn('Error', error.message)
-	console.warn(error.config)
-	return statusIsValid
+function checkStatus(res, error, log = true) {
+	if (res.status >= 200 && status < 300) {
+		if (log) {
+			console.warn(res.data)
+			console.warn(res.status)
+			console.warn(res.headers)
+		}
+		return true
+	} else {
+		if (log) {
+			if (error.response) {
+				console.warn(error.response.data)
+				console.warn(error.response.status)
+				console.warn(error.response.headers)
+			} else if (error.request) console.warn(error.request)
+			else console.warn('Error', error.message)
+			console.warn(error.config)
+		}
+		return false
+	}
 }
 
 function requestMenu(route) {
 	let req = {
 		res: '',
+		err: '',
 		valid: true
 	}
 	axios.get(route)
 		.then(res => req.res = res)
-		.catch(err => req.valid = handleError(err))
+		.catch(err => req.err = err)
+
+	req.valid = checkStatus(req.res, req.err)
+
 	return req
 }
 
 function updateMenu(data) {
 	let req = {
 		res: '',
+		err: '',
 		valid: true
 	}
 	axios.post('/publish', data)
-		.then(res => console.log(res))
-		.catch(err => req.valid = handleError(err))
-	return req.valid
+		.then(res => req.res = res)
+		.catch(err => req.err = err)
+
+	req.valid = checkStatus(req.res, req.err)
+
+	return req
 }
 
 exports.requestMenu = requestMenu
