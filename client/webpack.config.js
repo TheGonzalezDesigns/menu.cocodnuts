156289a36a3779b4d32b36b22403bebe6c80f7f8
hugo@thegonzalez.design <hugo@thegonzalez.design>
2017-12-26 20:57:09 -0800

    beautified

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,72 +4,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const PATHS = {
-    src: path.join( __dirname, '/src'),
-    dist: path.join( __dirname, '/dist'),
+	src: path.join(__dirname, '/src'),
+	dist: path.join(__dirname, '/dist'),
 };
 const FILES = {
-    entry: path.join(PATHS.src, '/scripts/index.js'),
-    template: path.join(PATHS.src, '/pug/index.pug'),
+	entry: path.join(PATHS.src, '/scripts/index.js'),
+	template: path.join(PATHS.src, '/pug/index.pug'),
 };
 
 module.exports = {
-    entry: FILES.entry,
-    output: {
-        path: PATHS.dist,
-        filename: 'index.js',
-    },
-    plugins: [
+	entry: FILES.entry,
+	output: {
+		path: PATHS.dist,
+		filename: 'index.js',
+	},
+	plugins: [
         new HtmlWebpackPlugin({
-            minify: {
-                collapseWhitespace: true,
-            },
-            hash: true,
-            mobile: true,
-            template: FILES.template,
-            title: "Testing!"
-        })
+			minify: {
+				collapseWhitespace: true,
+			},
+			hash: true,
+			mobile: true,
+			template: FILES.template,
+			title: "Testing!"
+		})
     ],
-    module: {
-        rules: [
-            {
-                test: /\.styl$/,
-                loader: 'style-loader!css-loader!stylus-loader',
+	module: {
+		rules: [
+			{
+				test: /\.styl$/,
+				loader: 'style-loader!css-loader!stylus-loader',
             },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules|webpack.js/,
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules|webpack.js/,
             },
-            {
-                test: /\.js$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules|webpack.js/,
-                options: {
-                    fix: true
-                }
+			{
+				test: /\.js$/,
+				loader: 'eslint-loader',
+				exclude: /node_modules|webpack.js/,
+				options: {
+					fix: true
+				}
             }
         ],
-    },
-    plugins: [
+	},
+	plugins: [
         new webpack.LoaderOptionsPlugin({
-            options: {
-                stylus: {
-                    use: [poststylus(['autoprefixer', 'rucksack-css'])],
-                },
-            },
-        }),
+			options: {
+				stylus: {
+					use: [poststylus(['autoprefixer', 'rucksack-css'])],
+				},
+			},
+		}),
         new webpack.optimize.UglifyJsPlugin()
     ],
-    devServer: {
-        contentBase: PATHS.dist,
-        compress: true,
-        port: 8080,
-        stats: 'minimal',
-        open: true,
-    },
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.js'
-        }
-    }
+	devServer: {
+		contentBase: PATHS.dist,
+		compress: true,
+		port: 8080,
+		stats: 'minimal',
+		open: true,
+	},
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.js'
+		}
+	}
 };
