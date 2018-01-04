a79b3c933174af228e0452835b7c5e2e55142e21
hugo@thegonzalez.design <hugo@thegonzalez.design>
2018-01-04 12:17:54 -0800

    back to 8081

--- client/src/scripts/network/router.js
+++ client/src/scripts/network/router.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 let client = axios.create({
-	baseURL: 'http://localhost:8080'
+	baseURL: 'http://localhost:8081'
 })
 
 function validate(res) {
--- server/src/index.js
+++ server/src/index.js
@@ -19,4 +19,4 @@ server.post('/publish', (req, res) => {
 	}))
 })
 
-server.listen(8080, 'localhost')
+server.listen(8081, 'localhost')
