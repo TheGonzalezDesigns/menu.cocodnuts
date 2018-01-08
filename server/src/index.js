6dccca997d2c68b69f579b6d278d0ab213f11108
hugo@thegonzalez.design <hugo@thegonzalez.design>
2018-01-07 19:59:55 -0800

    deleted extra superior traversion

--- server/src/index.js
+++ server/src/index.js
@@ -5,14 +5,14 @@ const morgan = require('morgan')
 const handle = require('./handle.js')
 const server = express()
 
-const public = '../../client/public'
+const public = '../client/public/' //./ = menu.cocodnuts/server/
 
 server.use(morgan('combined'))
 server.use(bodyParser.json())
 server.use(cors())
 server.use(express.static(public))
-server.set('trust proxy', true);
-server.set('trust proxy', 'loopback');
+server.set('trust proxy', true)
+server.set('trust proxy', 'loopback')
 
 server.get('/getData', (req, res) => {
 	res.send(handle.get())
