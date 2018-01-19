45f2bbedf05354acb2d98762a7d44f12c558d458
hugo@thegonzalez.design <hugo@thegonzalez.design>
2018-01-18 23:37:00 -0800

    added cartel

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
+const dynamix = require('./dynamix')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
-const server = express()
 const path = require('path')
-const dynamix = require('./dynamix')
+const cors = require('cors')
+const {
+	cartel
+} = require('./cartel')
+
 const source = path.resolve(__dirname, '../../client/public/')
 const index = path.join(source, '/index.html')
-
+const server = express()
 const port = dynamix.port
 
 server.use(morgan('combined'))
@@ -20,6 +23,12 @@ server.set('trust proxy', 'loopback')
 
 server.get('/', (req, res) => {
 	res.sendFile(index)
+})
+
+server.post('/', async (req, res) => {
+	console.log('Recieved POST\n', req.body)
+	const data = await cartel(req.body)
+	res.send(data)
 })
 
 server.listen(port, console.warn(`Listening on ${port}`))
